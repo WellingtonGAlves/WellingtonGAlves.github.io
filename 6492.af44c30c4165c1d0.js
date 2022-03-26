12ba7d02ceeeb98929490cff795586fab080db0e
Wellington <wellington_999@live.com>
2022-03-26 08:13:00 -0300

    first commit test

---
+++ 6492.af44c30c4165c1d0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[6492],{6492:(a,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var o=s(7697),c=s(6475),e=s(7506);const n=class{constructor(t){(0,o.r)(this,t)}render(){const t=(0,c.b)(this);return(0,o.h)(o.H,{class:(0,e.c)(this.color,{[t]:!0})},(0,o.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
